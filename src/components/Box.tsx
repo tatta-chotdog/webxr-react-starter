1e21cc62af4784a257a2f79f6489365d8efe2590
tatta-chotdog <tatta875dev@gmail.com>
2025-04-04 15:17:42 +0900

    fix: remove useEffect

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Vector3 } from "three";
-import { useXR } from "@react-three/xr";
 import { useColorChange } from "../hooks/useColorChange";
 import { useRotation } from "../hooks/useRotation";
 import { DebugPanel } from "./DebugPanel";
@@ -9,16 +8,8 @@ export const Box = () => {
   const { currentColor, changeBoxColor, setCurrentColor } = useColorChange();
   const [rotateSpeed, setRotateSpeed] = useState<number>(0.5);
   const meshRef = useRotation(rotateSpeed);
-  const { session } = useXR();
   const [position, setPosition] = useState<Vector3>(new Vector3(0, 1.5, -1));
   const [scale, setScale] = useState<Vector3>(new Vector3(0.3, 0.3, 0.3));
-
-  useEffect(() => {
-    if (session) {
-      session.addEventListener("selectstart", changeBoxColor);
-      return () => session.removeEventListener("selectstart", changeBoxColor);
-    }
-  }, [session, changeBoxColor]);
 
   return (
     <>
