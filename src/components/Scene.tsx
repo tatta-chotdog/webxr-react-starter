bb103cf39d8a34a4948ba9a490cf8d04e6daeee8
tatta-chotdog <tatta875dev@gmail.com>
2025-04-04 14:06:10 +0900

    fix: remove environment

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,14 +1,8 @@
-import { Environment } from "@react-three/drei";
 import { Box } from "./Box";
 
 export const Scene = () => {
   return (
     <>
-      <Environment preset="apartment" background blur={0.5} />
-      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
-        <planeGeometry args={[20, 20]} />
-        <meshStandardMaterial color="#f0f0f0" />
-      </mesh>
       <Box />
       <ambientLight intensity={0.5} />
       <directionalLight position={[5, 5, 5]} intensity={1} />
